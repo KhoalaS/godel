8beab7f88f6df7155513797c45323392c011137e
KhoalaS <khoalekappa@gmail.com>
2025-08-27 00:13:08 +0200

    add Edge type

--- pkg/pipeline/graph.go
+++ pkg/pipeline/graph.go
@@ -1,16 +1,34 @@
 package pipeline
 
 type Graph struct {
+	Edges    []Edge             `json:"edges"`
 	Nodes    map[string]*Node   `json:"nodes"`
 	Incoming map[string][]*Node `json:"incoming"`
 	Outgoing map[string][]*Node `json:"outgoing"`
 }
 
+type Edge struct {
+	Target       string `json:"target"`
+	Source       string `json:"source"`
+	SourceHandle string `json:"sourceHandle"`
+	TargetHandle string `json:"targetHandle"`
+}
+
 func NewGraph() *Graph {
 	return &Graph{
+		Edges:    []Edge{},
 		Nodes:    make(map[string]*Node),
 		Incoming: make(map[string][]*Node),
 		Outgoing: make(map[string][]*Node),
+	}
+}
+
+func applyInputs(graph *Graph, node *Node) {
+	for _, e := range graph.Edges {
+		if e.Target != node.Id {
+			continue
+		}
+		node.Inputs[e.TargetHandle] = graph.Nodes[e.Source].Outputs[e.TargetHandle]
 	}
 }
 
