55499904afce839c1e50b89754b627a6a002ce4d
KhoalaS <khoalekappa@gmail.com>
2025-08-30 21:51:22 +0200

    remove example data

--- ui/src/components/PipelineBuilder.vue
+++ ui/src/components/PipelineBuilder.vue
@@ -12,78 +12,10 @@ const { addNodes, onConnect, addEdges, screenToFlowCoordinate } = useVueFlow()
 // you can find many examples of how to create these custom components in the examples page of the docs
 
 // these are our nodes
-const nodes = ref<Node<{ label: string; hello?: string }>[]>([
-  // an input node, specified by using `type: 'input'`
-  {
-    id: '1',
-    position: { x: 250, y: 5 },
-    // all nodes can have a data object containing any data you want to pass to the node
-    // a label can property can be used for default nodes
-    data: { label: 'Node 1' },
-    connectable: true,
-  },
-
-  // default node, you can omit `type: 'default'` as it's the fallback type
-  {
-    id: '2',
-    position: { x: 100, y: 100 },
-    data: { label: 'Node 2' },
-  },
-
-  // An output node, specified by using `type: 'output'`
-  {
-    id: '3',
-    type: 'output',
-    position: { x: 400, y: 200 },
-    data: { label: 'Node 3' },
-  },
-
-  // this is a custom node
-  // we set it by using a custom type name we choose, in this example `special`
-  // the name can be freely chosen, there are no restrictions as long as it's a string
-  {
-    id: '4',
-    position: { x: 400, y: 200 },
-    data: {
-      label: 'Node 4',
-      hello: 'world',
-    },
-    connectable: true,
-  },
-])
+const nodes = ref<Node<{ label: string; hello?: string }>[]>([])
 
 // these are our edges
-const edges = ref<Edge[]>([
-  // default bezier edge
-  // consists of an edge id, source node id and target node id
-  {
-    id: 'e1->2',
-    source: '1',
-    target: '2',
-  },
-
-  // set `animated: true` to create an animated edge path
-  {
-    id: 'e2->3',
-    source: '2',
-    target: '3',
-    animated: true,
-  },
-
-  // a custom edge, specified by using a custom type name
-  // we choose `type: 'special'` for this example
-  {
-    id: 'e3->4',
-    type: 'step',
-    source: '3',
-    target: '4',
-
-    // all edges can have a data object containing any data you want to pass to the edge
-    data: {
-      hello: 'world',
-    },
-  },
-])
+const edges = ref<Edge[]>([])
 onMounted(async () => {
   await pipelineStore.init()
 })
