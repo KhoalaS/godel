1d6712284eb62af786d5259c4de540291693ca94
KhoalaS <khoalekappa@gmail.com>
2025-08-30 22:32:36 +0200

    server fs access to vue98

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,16 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    envDir: './',
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    server: {
+      fs: {
+        allow: [`${env.VITE_SOURCE_DIR}godel`, `${env.VITE_SOURCE_DIR}vue-98`, './src'],
+      },
     },
-  },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
