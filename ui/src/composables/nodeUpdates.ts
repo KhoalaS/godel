e9cc875d1cbc6809364c2e5c9b81cdb1d72769c9
KhoalaS <khoalekappa@gmail.com>
2025-09-20 16:29:39 +0200

    formatting

--- ui/src/composables/nodeUpdates.ts
+++ ui/src/composables/nodeUpdates.ts
@@ -1,8 +1,7 @@
 import type { NodeIO, PipelineNode } from '@/models/Node'
 import { FunctionRegistry } from '@/registries/InputHook'
 import type { PipelineStore } from '@/stores/pipeline'
-import { useNodeConnections, type NodeProps, type VueFlowStore } from '@vue-flow/core'
-import { nextTick } from 'vue'
+import { useNodeConnections, type NodeProps } from '@vue-flow/core'
 
 export function useNodeUpdates(props: NodeProps<PipelineNode>, pipelineStore: PipelineStore) {
   const vueFlow = pipelineStore.vueFlow
--- ui/src/error/DefaultErrorService.ts
+++ ui/src/error/DefaultErrorService.ts
@@ -1,5 +1,8 @@
 import type { ErrorService } from './ErrorService'
 
+/**
+ * Default service for error handling. Just logs the error to the console.
+ */
 export const DefaultErrorService: ErrorService = {
   handleError: (error, info) => console.error(error, info),
 }
