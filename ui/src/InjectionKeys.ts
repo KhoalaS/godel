4d4856417b969ba509da552d9d3fb3f51208a463
KhoalaS <khoalekappa@gmail.com>
2025-11-02 10:55:43 +0100

    remove injectionkey

--- ui/src/InjectionKeys.ts
+++ ui/src/InjectionKeys.ts
@@ -1,9 +1,7 @@
 import type { InjectionKey } from 'vue'
-import type { ErrorService } from './error/ErrorService'
 import type { HookFunctionService } from './inputhooks/HookFunctionService'
 import type { NotificationService } from './services/NotificationService'
 
-export const ErrorServiceKey = Symbol('ErrorService') as InjectionKey<ErrorService>
 export const HookFunctionServiceKey = Symbol(
   'HookFunctionService',
 ) as InjectionKey<HookFunctionService>
